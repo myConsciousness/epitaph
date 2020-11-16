654b69fa8ed3629d605be7c0428927f62d58f7dc
Kato Shinya <yourdream28@gmail.com>
2020-11-16 17:33:00 +0900

    add

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"org.thinkit.common.regex","l":"FluentRegex"},{"p":"org.thinkit.common.regex.catalog","l":"RegexPattern"}];updateSearchResults();
