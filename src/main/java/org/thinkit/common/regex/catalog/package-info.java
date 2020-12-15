2c5277499801453e1ede6b518040b6b6de172ed5
Kato Shinya <kato.shinya.dev@gmail.com>
2020-12-15 14:39:35 +0900

    update

--- src/main/java/org/thinkit/common/regex/catalog/package-info.java
+++ src/main/java/org/thinkit/common/regex/catalog/package-info.java
@@ -1,4 +1,7 @@
 /**
  * The package for managing catalogs.
+ *
+ * @author Kato Shinya
+ * @since 1.0.0
  */
 package org.thinkit.common.regex.catalog;
--- src/main/java/org/thinkit/common/regex/package-info.java
+++ src/main/java/org/thinkit/common/regex/package-info.java
@@ -37,5 +37,8 @@
  * epitaph.group();
  * </code>
  * </pre>
+ *
+ * @author Kato Shinya
+ * @since 1.0.0
  */
 package org.thinkit.common.regex;
